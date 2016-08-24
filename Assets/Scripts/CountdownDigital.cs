5f59e6cb539d0d3b61099454aba8f219db8ffa73
Drew Wiskus <andrewwiskus@gmail.com>
2016-08-23 20:52:42 -0500

    first unity project. p19 in cookbook

---
+++ Assets/Scripts/CountdownDigital.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class CountdownDigital : MonoBehaviour {
+	private Text textClock;
+
+	private float countdownTimerDuration;
+	private float countdownTimerStartTime;
+
+
+	// Use this for initialization
+	void Start () {
+		textClock = GetComponent<Text>();
+		CountdownTimerReset(30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string timerMessage = "Countdown Finished";
+		int timeLeft = (int)CountdownTimerSecondsRemaining();
+
+		if(timeLeft > 0)
+			timerMessage = "Countdown: " + LeadingZero( timeLeft );
+
+		textClock.text = timerMessage;
+	
+	}
+
+	private void CountdownTimerReset (float delayInSeconds) {
+		countdownTimerDuration = delayInSeconds;
+		countdownTimerStartTime = Time.time;
+	}
+
+	private float CountdownTimerSecondsRemaining (){
+		float elapsedSeconds = Time.time - countdownTimerStartTime;
+		float timeLeft = countdownTimerDuration - elapsedSeconds;
+		return timeLeft;
+	}
+
+	private string LeadingZero (int n){
+		return n.ToString ().PadLeft (2, '0'); 
+	}
+
+
+}
---
+++ Assets/Scripts/FadeOut.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour {
+	private CountdownTimer countdownTimer;
+	private Text textUI;
+	private int fadeDuration = 5;
+	private bool fading = false;
+	// Use this for initialization
+	void Start () {
+		textUI = GetComponent<Text>();
+		countdownTimer = GetComponent<CountdownTimer>();
+		StartFading(fadeDuration);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fading){
+			float alphaRemaining = countdownTimer.GetProportionTimeRemaining();
+			print (alphaRemaining);
+			Color c = textUI.material.color;
+			c.a = alphaRemaining;
+			textUI.material.color = c;
+
+			if (alphaRemaining < 0.01)
+				fading = false;
+		}
+	}
+
+	public void StartFading (int timerTotal){
+		countdownTimer.ResetTimer(timerTotal);
+		fading = true;
+	}
+
+		
+
+}
